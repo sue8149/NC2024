dce6e31837c1462ea105ffa7899329dc78daa4bf
sue8149 <lishuny1989@hotmail.com>
2025-02-23 23:56:28 +0800

    Add files via upload

--- eTNBC project.R
+++ eTNBC project.R
@@ -1,31 +1,27 @@
-
+# NC2024
+R code
 library(XLConnect)
 library(ggplot2)
 library(survival)
 library(survminer)
 library(pROC)
 library(ggsci)
-
-#survival analysis
+library(OptimalCutpoints)
+# Survival analysis
 book <- loadWorkbook("survivalNAC.xlsx")
-breast <- readWorksheet(book, sheet=10)
-# survival project
-
-fit <- survfit(Surv(DFS,Relapse) ~ ctDNA.burden,  
+breast <- readWorksheet(book, sheet=10) 
+str(breast)
+fit <- survfit(Surv(DFS,Relapse) ~ ctDNA.burden,  # 创建生存对象 
                data = breast) 
 fit <- survfit(Surv(DRFS,Metastasis) ~ ctDNA.burden,  # 创建生存对象 
                data = breast) 
+fit2 <- survfit(Surv(DFS,Relapse) ~ pcr,  # 创建生存对象 
+                data = breast) 
 fit3 <- survfit(Surv(DFS,Relapse) ~ combined, 
-                data = breast)# 创建生存对象 
-fit3 <- survfit(Surv(DRFS,Metastasis) ~ combined, 
-                data = breast)# 创建生存对象 
+                data = breast)
+ggsurvplot(fit, data = breast,pval = TRUE, risk.table = TRUE, palette = "lancet",legend="bottom")         
 
-ggsurvplot(fit, data = breast,pval = TRUE, risk.table = TRUE, palette = "lancet",legend="bottom")
-ggsurvplot(fit3, data = breast,pval = TRUE, risk.table = TRUE, legend="bottom", palette = c("#42B540","#00468B","#0099B4","#ED0000"))
-
-#ROC analysis
-#---新辅助阈值的ROC曲线
-library(pROC)
+# ROC analysis
 book2 <- loadWorkbook("ROC.xlsx")
 d <- readWorksheet(book2,sheet = 1)
 roc_1st <- roc(d$Relapse, d$VAF, plot = TRUE,print.thres=TRUE)
@@ -33,13 +29,16 @@ auc(roc_1st)
 ci.auc(roc_1st)
 g_roc <-ggroc(roc_1st, color= "red")+geom_abline(aes(x=1, y=1), intercept = 1, alpha=0.5, lty=2)+theme_classic()
 
-#OptimalCutpoints package
-library(OptimalCutpoints)
+# OptimalCutpoints
 cutpoint <- optimal.cutpoints(X = "VAF", status = "Relapse", tag.healthy = 0, methods = "MaxDOR", data = d, 
                               pop.prev = NULL, control = control.cutpoints(), ci.fit = TRUE)
 cutpoint <- optimal.cutpoints(X = "VAF", status = "Relapse", tag.healthy = 0, methods = "MaxProdSpSe", data = d, 
                               pop.prev = NULL, control = control.cutpoints(CFN=3), ci.fit = TRUE)
-cutpoint <- optimal.cutpoints(X = "VAF", status = "Relapse", tag.healthy = 0, methods = "MinPvalue", data = d, 
+cutpoint <- optimal.cutpoints(X = "VAF", status = "Relapse", tag.healthy = 0, methods = "Youden", data = d, 
                               pop.prev = NULL, control = control.cutpoints(CFN=3), ci.fit = TRUE)
+cutpoint <- optimal.cutpoints(X = "VAF", status = "Relapse", tag.healthy = 0, methods = "MinPvalue", data = d, 
+                              pop.prev = NULL, control = control.cutpoints(CFN=3), ci.fit = TRUE)      
+cutpoint <- optimal.cutpoints(X = "VAF", status = "Relapse", tag.healthy = 0, methods = "MinPvalue", data = d, 
+                              pop.prev = NULL, control = control.cutpoints(CFN=3), ci.fit = TRUE)    
 summary(cutpoint)
 
